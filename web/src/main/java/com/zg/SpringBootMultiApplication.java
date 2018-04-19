d703113dee15a57c19683f9e86a3391eed35a010
zgyy <695466133@qq.com>
2018-04-19 08:28:30 +0800

    build1

--- web/src/main/java/com/zg/SpringBootMultiApplication.java
+++ web/src/main/java/com/zg/SpringBootMultiApplication.java
@@ -1,7 +1,10 @@
+package com.zg;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootMultiApplication {
     public static void main(String[] args) { SpringApplication.run(SpringBootMultiApplication.class, args); }
 }
--- web/src/main/java/com/zg/controller/UserController.java
+++ web/src/main/java/com/zg/controller/UserController.java
@@ -3,6 +3,7 @@ package com.zg.controller;
 import com.zg.service.UserService;
 import com.zg.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @RequestMapping(value = "/login")
     public UserVo doLogin(@RequestParam String username, @RequestParam String password) {
         return userService.doLogin(username);
     }
